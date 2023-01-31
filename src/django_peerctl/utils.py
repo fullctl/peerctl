2fa4d9e5cca8f17f7dd93298b1a3f0992ae399f4
Stefan Pratter <stefan@20c.com>
2023-01-31 17:23:07 +0000

    more org/asn ownership fixes

--- src/django_peerctl/utils.py
+++ src/django_peerctl/utils.py
@@ -1,15 +1,23 @@
 from django_peerctl.models import Network
+from django.db import IntegrityError
+from django_peerctl.exceptions import ASNClaimed
 
 
 def get_network(asn, org):
-    return Network.get_or_create(asn, org)
-
+    try:
+        return Network.get_or_create(asn, org)
+    except IntegrityError:
+        # another org already claimed this asn
+        raise ASNClaimed()
 
 def verified_asns(perms, org, require_device=True):
     for permission in perms.pset.permissions.values():
         if permission.namespace.match(["verified", "asn"]):
             asn = int(permission.namespace[2])
-            net = get_network(asn, org)
+            try:
+                net = get_network(asn, org)
+            except ASNClaimed:
+                continue
             if require_device and not net.devices:
                 continue
             yield net
