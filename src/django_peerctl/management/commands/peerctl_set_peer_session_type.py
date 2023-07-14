9ce267ce97840f93d240fe23e5f9c6c1e2f1b9c3
Jason Carvalho <jason@20c.com>
2023-07-14 11:57:54 +0000

    fix logic
    - check if port exists
    - add log_info if peer_session_type is changed

--- src/django_peerctl/management/commands/peerctl_set_peer_session_type.py
+++ src/django_peerctl/management/commands/peerctl_set_peer_session_type.py
@@ -15,16 +15,27 @@ class Command(CommandInterface):
 
         for session in qset:
             port = Port().first(id=session.port.id)
+            if not port or port.id == 0:
+                continue
 
             pni_regex = re.compile(r"\bPNI\b", re.IGNORECASE)
+            new_peer_session_type = ""
             if port.is_ixi:
-                session.peer_session_type = "ixp"
+                new_peer_session_type = "ixp"
             # if "PNI" or "pni" in port.virtual_port_description:
             elif port.virtual_port_description and pni_regex.search(
                 port.virtual_port_description
             ):
-                session.peer_session_type = "pni"
+                new_peer_session_type = "pni"
             else:
-                session.peer_session_type = "transit"
+                new_peer_session_type = "transit"
 
+            if new_peer_session_type != session.peer_session_type:
+                self.log_info(
+                    f"Changing peer_session_type of {session} from {session.peer_session_type} to {new_peer_session_type}"
+                )
+                session.peer_session_type = new_peer_session_type
+                session.save()
+
+            session.peer_session_type
             session.save()
